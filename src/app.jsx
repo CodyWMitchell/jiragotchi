f4a1a51d4ae48ad77b2f04d4debde644e5b60604
Cody Mitchell <comitche@redhat.com>
2024-06-07 16:44:38 -0400

    create JQL request (cors error)

--- src/app.jsx
+++ src/app.jsx
@@ -17,13 +17,41 @@ const axiosAgent = axios.create({
   }
 });
 
+const getJiraJQL = async (jqlArgs) => {
+  try {
+    const response = await axiosAgent.get(
+      encodeURI(`/search?jql=${jqlArgs}`)
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
 const App = () => {
-    const [response, setResponse] = React.useState(null);
+    // sprint total time, remaining days in sprint, point total, point remaining, tickets total, tickets remaining, current tickets
+    const [sprintData, setSprintData] = React.useState(null);
+
+    const getUserData = async () => {
+      // Get user's sprint total time, remaining days in sprint, point total, point remaining, tickets total, tickets remaining, current tickets
+      const userTickets = await getJiraJQL(
+        `assignee = "${process.env.REACT_APP_JIRA_USERNAME}" AND resolution is EMPTY`
+      );
+      return userTickets;
+    };
+
+    React.useEffect(() => {
+      getUserData().then((data) => {
+        setSprintData(data);
+      });
+    });
 
     return (
       <>
-        <h1>RESPONSE FROM REQUEST TO {process.env.REACT_APP_BASE_URL}:</h1>
-        {response || 'No response yet'}
+        <h1>RESPONSE FROM REQUEST {process.env.REACT_APP_BASE_URL}:</h1>
+        {JSON.stringify(sprintData || 'No response yet')}
       </>
     );
 };
